48424ef93a8202a82dff5d7f2b46f139f55948fa
Alex Dauenhauer <alexdauenhauer17@gmail.com>
2019-11-23 16:20:03 -0600

    import tensorrt

--- scripts/label_image.py
+++ scripts/label_image.py
@@ -10,6 +10,7 @@ import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
+import tensorflow.contrib.tensorrt
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
